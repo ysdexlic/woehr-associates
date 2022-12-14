8e366f48c0731934827a2fa1e8155b0fd4e2269f
David Thompson <david.arthur.thompson@gmail.com>
2022-12-14 16:38:26 +0000

    update icons and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -7,10 +8,10 @@ function App() {
     <div className="app">
       <header className="app-header">
         <img src={logo} className="app-logo" alt="logo" />
-        <h1 className="uppercase">Woehr Associates</h1>
+        <h1>Woehr Associates</h1>
         <h2>Founded in 1956</h2>
-        <h3 className="header-description uppercase">
-          Employee Selection | Career Counselling
+        <h3 className="header-description">
+          Employee Selection & Career Counselling
         </h3>
       </header>
       <main>
@@ -39,7 +40,7 @@ function App() {
               <p>
                 Peter Kolson, Psy.D has worked as a consultant for Woehr
                 Associates for over 40 years. He earned his doctorate at
-                Hahnemann University
+                Hahnemann University.
               </p>
             </div>
           </div>
@@ -47,8 +48,14 @@ function App() {
         <footer>
           <h3>Please contact Amy Bobb, Office Manager with any questions.</h3>
           <div>
-            <h3>856-427-0776</h3>
-            <h3>abobb@woehrassociates.com</h3>
+            <a href="tel:856-427-0776">
+              <i className="fa-solid fa-phone"></i>
+              856-427-0776
+            </a>
+            <a href="mailto:abobb@woehrassociates.com">
+              <i className="fa-regular fa-envelope"></i>
+              abobb@woehrassociates.com
+            </a>
           </div>
         </footer>
       </main>
