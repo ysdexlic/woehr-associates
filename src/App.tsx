c2aad54ee1d2a61b49d97b3f38718d691054653e
David Thompson <david.arthur.thompson@gmail.com>
2022-12-16 18:32:19 +0000

    final

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,8 @@ function App() {
       <header className="app-header">
         <img src={logo} className="app-logo" alt="logo" />
         <h1>Woehr Associates</h1>
-        <h2>Founded in 1956</h2>
-        <h3 className="header-description">
-          Employee Selection & Career Counselling
-        </h3>
+        <h2>Founded in 1957</h2>
+        <h3 className="header-description">Employee Selection & Assesment</h3>
       </header>
       <main>
         <div className="content">
@@ -20,27 +18,27 @@ function App() {
             <p>
               Woehr Associates offers a detailed, individualized assessment of
               candidates under consideration for individual contributor,
-              supervisory or management roles and also provides assessments of
-              current employees for promotion. We also offer career guidance.
-              The evaluations provide a comprehensive assessment of relevant
-              competencies along with experience-tested recommendations.
+              supervisory, management, or executive roles and also provides
+              assessments of current employees for promotion. The evaluations
+              provide a comprehensive assessment of relevant competencies along
+              with experience-tested recommendations.
             </p>
           </div>
           <div className="people">
             <div>
-              <h3>Leslie Woehr Tuttle, PSY.D.</h3>
+              <h3>Leslie Woehr Tuttle, PsyD</h3>
               <p>
-                Leslie Woehr Tuttle, Psy.D., is President of Woehr Associates,
+                Leslie Woehr Tuttle, PsyD, is President of Woehr Associates,
                 where she has worked for over 40 years. She earned her Doctorate
                 in Psychology at Hahnemann University.
               </p>
             </div>
             <div>
-              <h3>Peter Kolson, PSY.D.</h3>
+              <h3>Peter Kolson, PsyD</h3>
               <p>
-                Peter Kolson, Psy.D has worked as a consultant for Woehr
-                Associates for over 40 years. He earned his doctorate at
-                Hahnemann University.
+                Peter Kolson, PsyD, has worked as a consultant for Woehr
+                Associates for over 40 years. He earned his Doctorate in
+                Psychology at Hahnemann University.
               </p>
             </div>
           </div>
